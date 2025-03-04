c4a8f21517fedd3eb1ea46ec5f739d02d5bf449e
nullmastermind <thenullmastermind@gmail.com>
2025-03-04 14:44:09 +0700

    fix: implement network reconnection on failed requests
    - Added `reconnect` function to handle network interface reset
    - Updated `.gitignore` to include `network.txt`
    - Modified `main` function to call `reconnect` on request failure
    - Introduced delay after reconnection attempt to stabilize connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import axios from "axios";
 import axiosRetry from "axios-retry";
+import { execSync } from "child_process";
+import fs from "fs";
 
 axiosRetry(axios, { retries: 3 });
+
+function reconnect() {
+  if (!fs.existsSync("network.txt")) {
+    fs.writeFileSync("network.txt", "Wi-Fi 3");
+  }
+  const networkName = JSON.stringify(fs.readFileSync("network.txt").toString("utf-8"));
+
+  execSync(`netsh interface set interface ${networkName} disable`);
+  execSync(`netsh interface set interface ${networkName} enable`);
+}
 
 async function main() {
   while (true) {
@@ -9,7 +21,11 @@ async function main() {
       await axios.get("https://api.ipify.org/");
 
       console.log("Connection OK");
-    } catch {}
+    } catch {
+      console.warn("Reconnect");
+      reconnect();
+      await new Promise((resolve) => setTimeout(resolve, 30000));
+    }
 
     await new Promise((resolve) => setTimeout(resolve, 3000));
   }
